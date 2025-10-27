d1d12f5a7a3c923e614c2f441c7e7ce6c2c76a22
Pranshu Gupta <pranshumg@gmail.com>
2025-10-27 23:58:57 +0530

    updated

--- Sorting and Searching/sum_of_two_values/code.cpp
+++ Sorting and Searching/sum_of_two_values/code.cpp
@@ -1,35 +1,31 @@
 /**
- *   author: pranshumg
+ *    author:  pranshumg
 **/
 #include <bits/stdc++.h>
 
 using namespace std;
 
 void run_case() {
-    int n, x;
-    cin >> n >> x;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        cin >> v[i];
+  int n, x;
+  cin >> n >> x;
+  vector<int> v(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+  }
+  map<int, int> mp;
+  for (int i = 0; i < n; ++i) {
+    if (mp.find(x - v[i]) != mp.end()) {
+      cout << mp[x - v[i]] << ' ' << i + 1 << '\n';
+      return;
     }
-    map<int, int> mp;
-    for (int i = 0; i < n; i++) {
-        if (mp.find(x - v[i]) != mp.end()) {
-            cout << mp[x - v[i]] << ' ' << i + 1;
-            return;
-        }
-        mp[v[i]] = i + 1;
-    }
-    cout << "IMPOSSIBLE";   
+    mp[v[i]] = i + 1;
+  }
+  cout << "IMPOSSIBLE\n";
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // int tests;
-    // cin >> tests;
-    // while (tests--) {
-        run_case();
-    // }
-    return 0;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  run_case();
+  return 0;
 }
