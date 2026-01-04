29775ccff1627065bd413f216b5d6a3ee387827b
Pranshu Gupta <pranshumg@gmail.com>
2026-01-04 12:34:20 +0530

    Missing NUmber

--- Introductory Problems/missing_number/code.cpp
+++ Introductory Problems/missing_number/code.cpp
@@ -1,19 +1,34 @@
-/**
- *   author: pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int64_t n, curr, sum = 0;
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    i64 n;
     cin >> n;
-    for (int64_t i = 0; i < n - 1; i++) {
-        cin >> curr;
-        sum += curr;
+    i64 s1 = (n * (n + 1)) >> 1, s2 = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        int cur;
+        cin >> cur;
+        s2 += cur;
     }
-    cout << ((n * (n + 1)) / 2) - sum;
+    cout << s1 - s2 << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
