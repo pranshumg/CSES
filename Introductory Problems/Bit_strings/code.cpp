c59e564a164249e999035c54518e705efa2609f0
Pranshu Gupta <pranshumg@gmail.com>
2026-01-04 19:50:43 +0530

    Bit Strings

--- Introductory Problems/Bit_strings/code.cpp
+++ Introductory Problems/Bit_strings/code.cpp
@@ -1,26 +1,34 @@
-/**
- *    author:  pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-#define MOD (int)1e9 + 7
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
 
-void run_case() {
-  int n;
-  cin >> n;
-  int ans = 1;
-  for (int i = 0; i < n; ++i) {
-    ans *= 2;
-    ans %= MOD;
-  }
-  cout << ans << '\n';
+#define all(x) (x).begin(), (x).end()
+
+constexpr int mod = (int)1e9 + 7;
+
+void solve() {
+    int n;
+    cin >> n;
+    i64 ans = 1;
+    for (int i = 0; i < n; ++i) {
+        ans *= 2, ans %= mod;
+    }
+    cout << ans << '\n';
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  run_case();
-  return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
