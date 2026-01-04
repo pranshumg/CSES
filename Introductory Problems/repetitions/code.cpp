7e3a34d9e55db905edb5e307637d9712d09e9350
Pranshu Gupta <pranshumg@gmail.com>
2026-01-04 12:43:52 +0530

    Repetitions

--- Introductory Problems/repetitions/code.cpp
+++ Introductory Problems/repetitions/code.cpp
@@ -1,24 +1,37 @@
-/**
- *   author: pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
     string s;
     cin >> s;
     int ans = 1, cnt = 1;
-    for (int i = 1; i < s.size(); i++) {
+    for (int i = 1; i < (int)s.size(); ++i) {
         if (s[i] == s[i - 1]) {
-            cnt++;
+            ++cnt;
         } else {
             cnt = 1;
         }
         ans = max(ans, cnt);
     }
-    cout << ans;
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
