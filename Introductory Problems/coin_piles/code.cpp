d6916e0d79e95f2608a05b8df2dd85b5f97f0289
Pranshu Gupta <pranshumg@gmail.com>
2026-01-04 13:08:27 +0530

    Coin Piles

--- Introductory Problems/coin_piles/code.cpp
+++ Introductory Problems/coin_piles/code.cpp
@@ -1,30 +1,28 @@
-/**
- *    author:  pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-void run_case() {
-  int64_t a, b;
-  cin >> a >> b;
-  if (a < b) {
-    swap(a, b);
-  }
-  if ((a + b) % 3 == 0 && a <= b * 2) {
-    cout << "YES\n";
-    return;
-  }
-  cout << "NO\n";
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    i64 a, b;
+    cin >> a >> b;
+    cout << ((a + b) % 3 == 0 && abs(a - b) <= min(a, b) ? "YES" : "NO") << '\n';    
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  int tests;
-  cin >> tests;
-  while (tests--) {
-    run_case();
-  }
-  return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
