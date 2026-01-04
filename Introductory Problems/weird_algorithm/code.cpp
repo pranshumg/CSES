321f6020d9a4642b1024b777b4828dc6ec2c3416
Pranshu Gupta <pranshumg@gmail.com>
2026-01-04 12:26:38 +0530

    Weird Algorithm

--- Introductory Problems/weird_algorithm/code.cpp
+++ Introductory Problems/weird_algorithm/code.cpp
@@ -1,24 +1,37 @@
-/**
- *   author: pranshumg
-**/
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int64_t n;
+
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    i64 n;
     cin >> n;
-    cout << n << " ";
+    cout << n << ' ';
     while (n != 1) {
-        if (n % 2 == 0) {
-            n /= 2;
-            cout << n << " ";
-        } else {
+        if (n & 1) {
+            cout << n * 3 + 1 << ' ';
             n = n * 3 + 1;
-            cout << n << " ";
+        } else {
+            cout << (n >> 1) << ' ';
+            n >>= 1;
         }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
     }
     return 0;
 }
