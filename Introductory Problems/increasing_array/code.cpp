ca9c27a8fa2a8c02acea473bbcaf1e8c63d37096
Pranshu Gupta <pranshumg@gmail.com>
2026-01-04 12:49:40 +0530

    Increasing Array

--- Introductory Problems/increasing_array/code.cpp
+++ Introductory Problems/increasing_array/code.cpp
@@ -1,26 +1,37 @@
-/**
- *   author: pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
     int n;
     cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
+    vector<i64> v(n);
+    i64 ans = 0;
+    for (int i = 0; i < n; ++i) {
         cin >> v[i];
-    } 
-    int64_t ans = 0;
-    for (int i = 1; i < n; i++) {
-        if (v[i] < v[i - 1]) {
+        if (i > 0 && v[i] < v[i - 1]) {
             ans += v[i - 1] - v[i];
             v[i] = v[i - 1];
         }
     }
-    cout << ans;
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
