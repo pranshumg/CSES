4411219656f08675a38c1db457e8a434367efa64
Pranshu Gupta <pranshumg@gmail.com>
2026-01-04 14:49:29 +0530

    Creating Strings

--- Introductory Problems/creating_strings/code.cpp
+++ Introductory Problems/creating_strings/code.cpp
@@ -1,25 +1,38 @@
-/**
- *   author: pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
     string s;
     cin >> s;
-    sort(s.begin(), s.end());
+    sort(all(s));
     vector<string> ans;
     int k = 0;
     do {
-        k++;
+        ++k;
         ans.emplace_back(s);
-    } while (next_permutation(s.begin(), s.end()));
+    } while (next_permutation(all(s)));
     cout << k << '\n';
-    for (auto it: ans) {
+    for (auto &it : ans) {
         cout << it << '\n';
+    } 
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
     }
     return 0;
 }
