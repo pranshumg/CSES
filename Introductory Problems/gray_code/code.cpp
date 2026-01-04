1df1dc64681926733e7517ecbd9d745eb16ee502
Pranshu Gupta <pranshumg@gmail.com>
2026-01-04 20:11:45 +0530

    Gray Code

--- Introductory Problems/gray_code/code.cpp
+++ Introductory Problems/gray_code/code.cpp
@@ -1,43 +1,46 @@
-/**
- *    author:  pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-string dec_to_bin(int gray, int n) {
-  string b;
-  while (gray > 0) {
-    int rem = gray % 2;
-    b = to_string(rem) + b;
-    gray /= 2;
-  }
-  while ((int)b.size() < n) {
-    b = '0' + b; 
-  }
-  return b;
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+string dec_to_bin(int g, int n) {
+    string s = "";
+    while (g) {
+        s = to_string(g % 2) + s;
+        g >>= 1;
+    }
+    while ((int)s.size() < n) {
+        s = '0' + s;
+    }
+    return s;
 }
 
 int g(int n) {
-  return n ^ (n >> 1);
+    return n ^ (n >> 1);
 }
 
-void run_case() {
-  int n;
-  cin >> n;
-  int total = 1 << n;
-  for (int i = 0; i < total; ++i) {
-    cout << dec_to_bin(g(i), n) << '\n';
-  }
+void solve() {
+    int n;
+    cin >> n;
+    for (int i = 0; i < (1 << n); ++i) {
+        cout << dec_to_bin(g(i), n) << '\n';
+    }
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  // int tests;
-  // cin >> tests;
-  // while (tests--) {
-    run_case();
-  // }
-  return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
