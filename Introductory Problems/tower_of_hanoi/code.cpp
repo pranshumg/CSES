7101431c37617a8b1b135f6c526fc42f36ac74bd
Pranshu Gupta <pranshumg@gmail.com>
2026-01-05 12:05:09 +0530

    Tower of Hanoi

--- Introductory Problems/tower_of_hanoi/code.cpp
+++ Introductory Problems/tower_of_hanoi/code.cpp
@@ -1,26 +1,39 @@
-/**
- *   author: pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-void move(int n, int left, int right, int middle) {
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void move(int n, int l, int r, int m) {
     if (n == 1) {
-        cout << left << " " << right << '\n'; 
+        cout << l << " " << r << '\n'; 
         return;
     }
-    move(n - 1, left, middle, right);
-    cout << left << " " << right << '\n';
-    move(n - 1, middle, right, left);
+    move(n - 1, l, m, r);
+    cout << l << " " << r << '\n';
+    move(n - 1, m, r, l);
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    cout << (1 << n) - 1 << '\n';
+    move(n, 1, 3, 2);
 }
 
 int main() {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int n;
-    cin >> n;
-    cout << pow(2, n) - 1 << '\n';
-    move(n, 1, 3, 2);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
