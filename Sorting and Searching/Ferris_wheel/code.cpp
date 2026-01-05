4aefdb851afc5f5f38d69b6595fca85e88b5cefa
Pranshu Gupta <pranshumg@gmail.com>
2026-01-05 12:28:39 +0530

    Ferris Wheel

--- Sorting and Searching/Ferris_wheel/code.cpp
+++ Sorting and Searching/Ferris_wheel/code.cpp
@@ -1,28 +1,42 @@
-/**
- *   author: pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n, x;
-    cin >> n >> x;
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n;
+    cin >> n;
+    i64 x;
+    cin >> x;
     vector<int> v(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
         cin >> v[i];
     }
-    sort(v.begin(), v.end());
-    int ans = 0, i = 0, j = n - 1;
-    while (i <= j) {
-        if (v[i] + v[j] <= x) {
-            i++;
+    sort(all(v));
+    int ans = 0, l = 0, r = n - 1;
+    while (l <= r) {
+        if (v[l] + v[r] <= x) {
+            ++l;
         }
-        j--;
-        ans++;
+        --r, ++ans;
     }
-    cout << ans;
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
