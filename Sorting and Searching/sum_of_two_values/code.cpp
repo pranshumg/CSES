c2e199f0bb3ed1a346c94d22957dd14ded7f588a
Pranshu Gupta <pranshumg@gmail.com>
2026-01-05 15:11:54 +0530

    Sum of Two Values

--- Sorting and Searching/sum_of_two_values/code.cpp
+++ Sorting and Searching/sum_of_two_values/code.cpp
@@ -1,31 +1,42 @@
-/**
- *    author:  pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-void run_case() {
-  int n, x;
-  cin >> n >> x;
-  vector<int> v(n);
-  for (int i = 0; i < n; ++i) {
-    cin >> v[i];
-  }
-  map<int, int> mp;
-  for (int i = 0; i < n; ++i) {
-    if (mp.find(x - v[i]) != mp.end()) {
-      cout << mp[x - v[i]] << ' ' << i + 1 << '\n';
-      return;
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n;
+    cin >> n;
+    i64 x;
+    cin >> x;
+    vector<i64> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
     }
-    mp[v[i]] = i + 1;
-  }
-  cout << "IMPOSSIBLE\n";
+    map<i64, int> mp;
+    for (int i = 0; i < n; ++i) {
+        if (mp.find(x - v[i]) != mp.end()) {
+            cout << mp[x - v[i]] << ' ' << i + 1 << '\n';
+            return;
+        }
+        mp[v[i]] = i + 1;
+    }
+    cout << "IMPOSSIBLE\n";
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  run_case();
-  return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
