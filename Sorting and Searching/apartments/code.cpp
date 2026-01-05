49ef81f8294570a2e944664027d2e3ca06534ed9
Pranshu Gupta <pranshumg@gmail.com>
2026-01-05 15:38:14 +0530

    Apartments

--- Sorting and Searching/apartments/code.cpp
+++ Sorting and Searching/apartments/code.cpp
@@ -1,34 +1,49 @@
-/**
- *   author: pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n, m, k;
-    cin >> n >> m >> k;
-    vector<int> v1(n), v2(m);
-    for (int i = 0; i < n; i++) {
-        cin >> v1[i];
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    i64 k;
+    cin >> k;
+    vector<i64> a(n), b(m);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
     }
-    for (int i = 0; i < m; i++) {
-        cin >> v2[i];
+    for (int i = 0; i < m; ++i) {
+        cin >> b[i];
     }
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end());
-    int i = 0, j = 0, ans = 0;
-    while (i < n && j < m) {
-        if (abs(v1[i] - v2[j]) <= k) {
-            ans++, i++, j++;
-        } else if (v2[j] < v1[i] - k) {
-            j++;
+    sort(all(a));
+    sort(all(b));
+    int l = 0, r = 0, ans = 0;
+    while (l < n && r < m) {
+        if (abs(a[l] - b[r]) <= k) {
+            ++ans, ++l, ++r;
+        } else if (b[r] - k > a[l]) {
+            ++l;
         } else {
-            i++;
+            ++r;
         }
     }
-    cout << ans;
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
