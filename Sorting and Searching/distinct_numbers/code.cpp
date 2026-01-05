ece3fd5212ea9ce386c1fc7c4ed62db1f29624af
Pranshu Gupta <pranshumg@gmail.com>
2026-01-05 12:08:58 +0530

    Distinct Numbers

--- Sorting and Searching/distinct_numbers/code.cpp
+++ Sorting and Searching/distinct_numbers/code.cpp
@@ -1,20 +1,34 @@
-/**
- *   author: pranshumg
-**/
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n, curr;
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n;
     cin >> n;
-    set<int> st;
-    for (int i = 0; i < n; i++) {
-        cin >> curr;
-        st.insert(curr);
+    map<int, int> mp;
+    for (int i = 0; i < n; ++i) {
+        int cur;
+        cin >> cur;
+        ++mp[cur];
     }
-    cout << st.size();
+    cout << (int)mp.size() << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
