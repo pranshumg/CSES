c190d37a9d1ace243df90907cc5ab6aeb11a19b9
Pranshu Gupta <pranshumg@gmail.com>
2026-01-11 15:59:29 +0530

    Exponentiation

---
+++ Mathematics/Exponentiation/code.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+constexpr int mod = (int)1e9 + 7;
+
+void solve() {
+    i64 a, b;
+    cin >> a >> b;
+    i64 ans = 1;
+    while (b) {
+        if (b & 1) {
+            ans = (ans * a) % mod;
+        }
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
