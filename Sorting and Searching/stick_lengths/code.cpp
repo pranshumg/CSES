c55a8f094be6e51e49b7102fe37222381d362146
Pranshu Gupta <pranshumg@gmail.com>
2026-02-01 14:58:59 +0530

    stick lengths

---
+++ Sorting and Searching/stick_lengths/code.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    sort(all(v));
+    int el = v[n / 2];
+    i64 ans = 0;
+    for (auto &it : v) {
+        ans += abs(it - el);
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
