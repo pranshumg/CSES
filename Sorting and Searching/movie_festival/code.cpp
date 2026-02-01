5682168cdf3d743d594840cbcb3456801e536c26
Pranshu Gupta <pranshumg@gmail.com>
2026-02-01 14:14:18 +0530

    movie festival

---
+++ Sorting and Searching/movie_festival/code.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i].first >> v[i].second;
+    }
+    sort(all(v), [](auto &a, auto &b) {
+        return a.second < b.second;
+    });
+    int f = -1, ans = 0;
+    for (auto &it : v) {
+        if (it.first >= f) {
+            ++ans;
+            f = it.second;
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
