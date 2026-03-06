365136600e9d2b5194b6f718d00daefc0aef36b2
Pranshu Gupta <pranshumg@gmail.com>
2026-03-07 02:30:27 +0530

    updated code

--- Introductory Problems/Bit_strings/code.cpp
+++ Introductory Problems/Bit_strings/code.cpp
@@ -1,34 +1,20 @@
+//    Author:  Pranshu Gupta
 #include <bits/stdc++.h>
 
 using namespace std;
 
-using i64 = int64_t;
-using u64 = uint64_t;
-using u32 = uint32_t;
-using i128 = __int128_t;
-using u128 = __uint128_t;
-
-#define all(x) (x).begin(), (x).end()
-
-constexpr int mod = (int)1e9 + 7;
-
-void solve() {
-    int n;
-    cin >> n;
-    i64 ans = 1;
-    for (int i = 0; i < n; ++i) {
-        ans *= 2, ans %= mod;
-    }
-    cout << ans << '\n';
-}
+const int md = int(1e9) + 7;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t = 1;
-    // cin >> t;
-    while (t--) {
-        solve();
-    }
-    return 0;
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  int ans = 1;
+  for (int i = 0; i < n; ++i) {
+    ans <<= 1;
+    ans %= md;
+  }
+  cout << ans << '\n';
+  return 0;
 }
