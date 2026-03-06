d8afdf36588332c14ab32a5e14fe0bd1705ecb12
Pranshu Gupta <pranshumg@gmail.com>
2026-03-07 02:22:39 +0530

    updated code

--- Introductory Problems/increasing_array/code.cpp
+++ Introductory Problems/increasing_array/code.cpp
@@ -1,37 +1,24 @@
+//    Author:  Pranshu Gupta
 #include <bits/stdc++.h>
 
 using namespace std;
 
-using i64 = int64_t;
-using u64 = uint64_t;
-using u32 = uint32_t;
-using i128 = __int128_t;
-using u128 = __uint128_t;
-
-#define all(x) (x).begin(), (x).end()
-
-void solve() {
-    int n;
-    cin >> n;
-    vector<i64> v(n);
-    i64 ans = 0;
-    for (int i = 0; i < n; ++i) {
-        cin >> v[i];
-        if (i > 0 && v[i] < v[i - 1]) {
-            ans += v[i - 1] - v[i];
-            v[i] = v[i - 1];
-        }
-    }
-    cout << ans << '\n';
-}
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t = 1;
-    // cin >> t;
-    while (t--) {
-        solve();
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int64_t> v(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+  }
+  int64_t ans = 0;
+  for (int i = 1; i < n; ++i) {
+    if (v[i] < v[i - 1]) {
+      ans += v[i - 1] - v[i];
+      v[i] = v[i - 1];
     }
-    return 0;
+  }
+  cout << ans << '\n';
+  return 0;
 }
