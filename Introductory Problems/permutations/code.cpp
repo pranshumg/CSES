27456d090a1cecc15aa45d5477101010cabef521
Pranshu Gupta <pranshumg@gmail.com>
2026-03-07 02:27:17 +0530

    updated code

--- Introductory Problems/permutations/code.cpp
+++ Introductory Problems/permutations/code.cpp
@@ -1,41 +1,27 @@
+//    Author:  Pranshu Gupta
 #include <bits/stdc++.h>
 
 using namespace std;
 
-using i64 = int64_t;
-using u64 = uint64_t;
-using u32 = uint32_t;
-using i128 = __int128_t;
-using u128 = __uint128_t;
-
-#define all(x) (x).begin(), (x).end()
-
-void solve() {
-    int n;
-    cin >> n;
-    if (n == 1) {
-        cout << n << '\n';
-        return;
-    }
-    if (n < 4) {
-        cout << "NO SOLUTION\n";
-        return;
-    }
-    for (int i = 2; i <= n; i += 2) {
-        cout << i << ' ';
-    }
-    for (int i = 1; i <= n; i += 2) {
-        cout << i << ' ';
-    }
-}
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t = 1;
-    // cin >> t;
-    while (t--) {
-        solve();
-    }
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  if (n == 1) {
+    cout << 1 << '\n';
     return 0;
+  }
+  if (n < 4) {
+    cout << "NO SOLUTION\n";
+    return 0;
+  }
+  for (int i = 2; i <= n; i += 2) {
+    cout << i << ' ';
+  }
+  for (int i = 1; i <= n; i += 2) {
+    cout << i << ' ';
+  }
+  cout << '\n';
+  return 0;
 }
