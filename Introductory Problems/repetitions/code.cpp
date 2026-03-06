cbace6f85cb535ffc6a5ae807bd1c552ad3a03c2
Pranshu Gupta <pranshumg@gmail.com>
2026-03-07 02:19:20 +0530

    updated code

--- Introductory Problems/repetitions/code.cpp
+++ Introductory Problems/repetitions/code.cpp
@@ -1,37 +1,21 @@
+//    Author:  Pranshu Gupta
 #include <bits/stdc++.h>
 
 using namespace std;
 
-using i64 = int64_t;
-using u64 = uint64_t;
-using u32 = uint32_t;
-using i128 = __int128_t;
-using u128 = __uint128_t;
-
-#define all(x) (x).begin(), (x).end()
-
-void solve() {
-    string s;
-    cin >> s;
-    int ans = 1, cnt = 1;
-    for (int i = 1; i < (int)s.size(); ++i) {
-        if (s[i] == s[i - 1]) {
-            ++cnt;
-        } else {
-            cnt = 1;
-        }
-        ans = max(ans, cnt);
-    }
-    cout << ans << '\n';
-}
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t = 1;
-    // cin >> t;
-    while (t--) {
-        solve();
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string s;
+  cin >> s;
+  int l = 0;
+  int ans = 0;
+  for (int r = 0; r < int(s.size()); ++r) {
+    if (s[l] != s[r]) {
+      l = r;
     }
-    return 0;
+    ans = max(ans, r - l + 1);
+  }
+  cout << ans << '\n';
+  return 0;
 }
