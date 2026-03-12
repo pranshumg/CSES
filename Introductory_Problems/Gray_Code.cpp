0761e948d78ed4973e2a1d5c263e1191a9a693b9
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:39:45 +0530

    gray code

---
+++ Introductory_Problems/Gray_Code.cpp
@@ -0,0 +1,29 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int g(int n) {
+  return n ^ (n >> 1);
+}
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  auto bin = [](int g, int n) -> string {
+    string res = "";
+    while (g) {
+      res += g % 2 ? '1' : '0';
+      g >>= 1;
+    }
+    reverse(res.begin(), res.end());
+    while (int(res.size()) < n) res = '0' + res;
+    return res;
+  };
+  int n;
+  cin >> n;
+  for (int i = 0; i < (1 << n); ++i) {
+    cout << bin(g(i), n) << '\n';
+  }
+  return 0;
+}
