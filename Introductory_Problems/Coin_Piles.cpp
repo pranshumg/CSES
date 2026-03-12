4be9852c23ac31f8e15c858703622363547847da
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:38:15 +0530

    coin piles

---
+++ Introductory_Problems/Coin_Piles.cpp
@@ -0,0 +1,17 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int64_t a, b;
+    cin >> a >> b;
+    cout << ((a + b) % 3 == 0 && abs(a - b) <= min(a, b) ? "YES" : "NO") << '\n';
+  }
+  return 0;
+}
