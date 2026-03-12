df60dc2ab78d56ce1e41f8232c3c3a4e73030ec2
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:31:33 +0530

    Weird Algorithm

---
+++ Introductory_Problems/Weird_Algorithm.cpp
@@ -0,0 +1,19 @@
+//    Author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int64_t n;
+  cin >> n;
+  cout << n << ' ';
+  while (n > 1) {
+    if (n & 1) n = n * 3 + 1;
+    else n >>= 1;
+    cout << n << ' ';
+  } 
+  cout << '\n';
+  return 0;
+}
