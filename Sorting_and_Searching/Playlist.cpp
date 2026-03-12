51f4fe0b0c23723ee09a9ea795bcf4135c0f7cce
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:50:25 +0530

    Playlist

---
+++ Sorting_and_Searching/Playlist.cpp
@@ -0,0 +1,27 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+  }
+  int l = 0;
+  int ans = 0;
+  map<int, int> mp;
+  for (int r = 0; r < n; ++r) {
+    while (l < r && mp.find(v[r]) != mp.end()) {
+      mp.erase(v[l++]);
+    }
+    ++mp[v[r]];
+    ans = max(ans, int(mp.size()));
+  }
+  cout << ans << '\n';
+  return 0;
+}
