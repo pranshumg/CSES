47622da77adee7fdf7342cefd273a2015c3d4b32
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:57:09 +0530

    counting divisors

---
+++ Mathematics/Counting_Divisors.cpp
@@ -0,0 +1,24 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  while (n--) {
+    int x;
+    cin >> x;
+    int cnt = 0;
+    for (int i = 1; i * i <= x; ++i) {
+      if (x % i == 0) {
+        ++cnt;
+        if (x / i != i) ++cnt;
+      }
+    }
+    cout << cnt << '\n';
+  }
+  return 0;
+}
