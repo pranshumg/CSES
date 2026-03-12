03f01b318cbe5072e10e135731f495e78d3d0a9c
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:47:46 +0530

    sum of two values

---
+++ Sorting_and_Searching/Sum_of_Two_Values.cpp
@@ -0,0 +1,25 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int64_t n, x;
+  cin >> n >> x;
+  vector<int64_t> v(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+  }
+  map<int64_t, int> mp;
+  for (int i = 0; i < n; ++i) {
+    if (mp.find(x - v[i]) != mp.end()) {
+      cout << mp[x - v[i]] << ' ' << i + 1 << '\n';
+      return 0;
+    }
+    mp[v[i]] = i + 1;
+  }
+  cout << "IMPOSSIBLE\n";
+  return 0;
+}
