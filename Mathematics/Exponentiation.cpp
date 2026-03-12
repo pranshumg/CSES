57c91e17ff7e84682926863fcac5266a0f21947c
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:56:14 +0530

    exponentation

---
+++ Mathematics/Exponentiation.cpp
@@ -0,0 +1,30 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int md = int(1e9) + 7;
+ 
+int main() {
+  ios::sync_with_stdio(false); 
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  while (n--) {
+    auto pow = [](int64_t a, int b) -> int {
+      int64_t ans = 1;
+      while (b) {
+        if (b & 1) {
+          ans = (ans * a) % md;
+        }
+        a = (a * a) % md;
+        b >>= 1;
+      }
+      return ans;
+    };
+    int a, b;
+    cin >> a >> b;
+    cout << pow(a, b) << '\n';
+  }
+  return 0;
+}
