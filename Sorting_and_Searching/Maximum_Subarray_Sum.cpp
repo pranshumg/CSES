5afad863b6dcb91966a5c17c2fc14f398b9d6811
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:48:23 +0530

    maximum subarray sum

---
+++ Sorting_and_Searching/Maximum_Subarray_Sum.cpp
@@ -0,0 +1,23 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int64_t> v(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+  }
+  int64_t ans = -1e9, sum = 0;
+  for (int64_t i : v) {
+    if (sum + i < i) sum = i;
+    else sum += i;
+    ans = max(ans, sum);
+  }
+  cout << ans << '\n';
+  return 0;
+}
