a0706b7ac38030da78a44a2b7dcf9dbf30103575
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:58:50 +0530

    Sliding window xor

---
+++ Sliding_Window_Problems/Sliding_Window_Xor.cpp
@@ -0,0 +1,31 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, k;
+  cin >> n >> k;
+  int x, a, b, c;
+  cin >> x >> a >> b >> c;
+  vector<int> v(n);
+  v[0] = x;
+  for (int i = 1; i < n; ++i) {
+    v[i] = (int64_t(a) * v[i - 1] + b) % c;
+  }
+  int64_t ans = 0, xr = 0;
+  int l = 0;
+  for (int r = 0; r < n; ++r) {
+    while (r - l + 1 > k) {
+      xr ^= v[l++];
+    }
+    xr ^= v[r];
+    if (r - l + 1 == k) {
+      ans ^= xr;
+    }
+  }
+  cout << ans << '\n';
+  return 0;
+}
