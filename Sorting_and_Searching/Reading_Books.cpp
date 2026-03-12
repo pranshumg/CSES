dcc382a1ef34dd732e5accade0f47ca0acbb8350
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:51:05 +0530

    Reading books

---
+++ Sorting_and_Searching/Reading_Books.cpp
@@ -0,0 +1,20 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  int64_t sum = 0;
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+    sum += v[i];
+  }
+  sort(v.begin(), v.end());
+  cout << max(sum, int64_t(v[n - 1] * 2)) << '\n';
+  return 0;
+}
