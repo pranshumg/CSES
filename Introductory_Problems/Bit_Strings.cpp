3e9855c572850d4e6faccea01d065030b2ff3277
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:37:25 +0530

    bit strings

---
+++ Introductory_Problems/Bit_Strings.cpp
@@ -0,0 +1,20 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int md = int(1e9) + 7;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  int ans = 1;
+  for (int i = 0; i < n; ++i) {
+    ans <<= 1;
+    ans %= md;
+  }
+  cout << ans << '\n';
+  return 0;
+}
