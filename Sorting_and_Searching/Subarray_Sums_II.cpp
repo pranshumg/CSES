f8d4e95bfdcd029008b034c53c5cde3dca775fe3
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:53:17 +0530

    subarray sum 2

---
+++ Sorting_and_Searching/Subarray_Sums_II.cpp
@@ -0,0 +1,25 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int64_t n, x;
+  cin >> n >> x;
+  vector<int64_t> v(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> v[i];
+  }
+  map<int64_t, int> mp;
+  ++mp[0];
+  int64_t sum = 0, ans = 0;
+  for (int i = 0; i < n; ++i) {
+    sum += v[i];
+    ans += mp[sum - x];
+    ++mp[sum];
+  }
+  cout << ans << '\n';
+  return 0;
+}
