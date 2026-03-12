54b2cecf469dc17838c27195f03bb9751d63b3f6
Pranshu Gupta <pranshumg@gmail.com>
2026-03-13 03:35:04 +0530

    Repetitons

---
+++ Introductory_Problems/Repetitions.cpp
@@ -0,0 +1,21 @@
+//    author:  Pranshu Gupta
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string s;
+  cin >> s;
+  int l = 0;
+  int ans = 0;
+  for (int r = 0; r < int(s.size()); ++r) {
+    if (s[l] != s[r]) {
+      l = r;
+    }
+    ans = max(ans, r - l + 1);
+  }
+  cout << ans << '\n';
+  return 0;
+}
