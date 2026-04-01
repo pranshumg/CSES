ee3127ec768f0e671a3eedcf98b2dce32a482ae7
Pranshu Gupta <pranshumg@gmail.com>
2026-04-01 23:57:02 +0530

    updated loop

--- Introductory_Problems/Coin_Piles.cpp
+++ Introductory_Problems/Coin_Piles.cpp
@@ -7,10 +7,10 @@ int main() {
   cin.tie(nullptr);
   int tc;
   cin >> tc;
-  while (tc--) {
+  while (tc--) [&]() -> void {
     int64_t a, b;
     cin >> a >> b;
     cout << ((a + b) % 3 == 0 && abs(a - b) <= min(a, b) ? "YES" : "NO") << '\n';
-  }
+  }();
   return 0;
 }
