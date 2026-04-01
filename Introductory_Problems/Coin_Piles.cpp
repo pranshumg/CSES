87a4d84f7df9020605409271c13e7d08cb25f1a3
Pranshu Gupta <pranshumg@gmail.com>
2026-04-01 15:29:51 +0530

    changed var

--- Introductory_Problems/Coin_Piles.cpp
+++ Introductory_Problems/Coin_Piles.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  int t;
-  cin >> t;
-  while (t--) {
+  int tc;
+  cin >> tc;
+  while (tc--) {
     int64_t a, b;
     cin >> a >> b;
     cout << ((a + b) % 3 == 0 && abs(a - b) <= min(a, b) ? "YES" : "NO") << '\n';
