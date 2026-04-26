56c763623bf2d843711bc40dd79d4a1e351aa49f
Pranshu Gupta <pranshumg@gmail.com>
2026-04-26 18:05:38 +0530

    initial commit

---
+++ Sorting_and_Searching/Distinct_Values_Subarrays_II.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    int64_t ans = 0;
+    map<int, int> mp;
+    int l = 0;
+    for (int r = 0; r < n; ++r) {
+        ++mp[v[r]];
+        while (int(mp.size()) > k) {
+            --mp[v[l]];
+            if (!mp[v[l]]) mp.erase(v[l]);
+            ++l;
+        }
+        ans += r - l + 1;
+    }
+    cout << ans << '\n';
+    return 0;
+}
