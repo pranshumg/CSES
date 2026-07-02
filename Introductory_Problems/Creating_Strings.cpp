c293bea57f56617c52bbfb549fe0468e360aeda1
Pranshu Gupta <pranshumg@gmail.com>
2026-07-03 02:54:30 +0530

    updated code

--- Introductory_Problems/Creating_Strings.cpp
+++ Introductory_Problems/Creating_Strings.cpp
@@ -7,14 +7,22 @@ int main() {
     cin.tie(nullptr);
     string s;
     cin >> s;
-    sort(s.begin(), s.end());
-    vector<string> res;
-    do {
-        res.push_back(s);
-    } while (next_permutation(s.begin(), s.end()));
+    set<string> res;
+    auto solve = [&](auto&& self, int idx) -> void {
+        if (idx == int(s.size())) {
+            res.insert(s);
+            return;
+        }
+        for (int i = idx; i < int(s.size()); i++) {
+            swap(s[idx], s[i]);
+            self(self, idx + 1);
+            swap(s[idx], s[i]);
+        }
+    };
+    solve(solve, 0);
     cout << int(res.size()) << '\n';
-    for (auto& a : res) {
-        cout << a << '\n';
+    for (string s : res) {
+        cout << s << '\n';
     }
     return 0;
 }
